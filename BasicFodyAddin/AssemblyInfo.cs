6bcd3962aaf36fc119b29de6c3b0ae4c0320760d
qkub <bostjan.kukovec@gmail.com>
2015-10-05 10:36:31 +0200

    readme info

--- BasicFodyAddin/AssemblyInfo.cs
+++ BasicFodyAddin/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("ExpandoIntelligizer")]
 [assembly: AssemblyProduct("ExpandoIntelligizer")]
-[assembly: AssemblyVersion("0.1.3")]
-[assembly: AssemblyFileVersion("0.1.3")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
